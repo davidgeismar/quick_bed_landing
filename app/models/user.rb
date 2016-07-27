5d568f517887fdefc0ee118c51e0365e9a3e58a3
David Geismar <davidgeismar@hotmail.fr>
2016-07-27 14:22:21 +0200

    more

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   validates :first_name,  presence: { message: 'Veuillez remplir votre prénom' }
   validates :last_name,   presence: { message: 'Veuillez remplir votre nom' }
-  validates :email, format: {
+  validates :email,presence: true,  format: {
     with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
     message: 'Veuillez renseigner un email valide'
   }
