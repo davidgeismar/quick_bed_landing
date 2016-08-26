1d9a4529e9f5b085824246864fbfc43d9d1eb3b3
David Geismar <davidgeismar@hotmail.fr>
2016-08-26 10:08:26 +0200

    email modif

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ApplicationMailer
   def new_subscription(user)
     @user = user  # Instance variable => available in view
 
-    mail(to: "pelevy@quickbed.org", subject: 'New subscription on landing')
+    mail(to: "pe.levy@quickbed.org", subject: 'New subscription on landing')
     # This will render a view in `app/views/user_mailer`!
   end
 end
